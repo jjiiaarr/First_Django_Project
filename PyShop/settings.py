a0baaebe46212ea9cb67d906d5a87d49f16cad55
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-08-13 12:32:43 +0800

    generate model, setup database and migrate

--- PyShop/settings.py
+++ PyShop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products.apps.ProductsConfig'
 ]
 
 MIDDLEWARE = [
@@ -70,17 +71,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'PyShop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
