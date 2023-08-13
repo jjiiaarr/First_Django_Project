2b1c27fc19d2ede6ccf51096cd6d0579465195a2
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-08-13 11:07:23 +0800

    add new function

--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return HT\
 
+def index(request):
+    return HttpResponse('Hello World')
+
+
+def new(request):
+    return HttpResponse('New Products')
